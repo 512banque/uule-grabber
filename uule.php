6b7089c49a7108c7d6e7c764a1906a08a6e23e47
Kévin RICHARD <kevin@rMBP.local>
2013-11-22 00:27:41 +0100

    Initial commit

---
+++ uule.php
@@ -0,0 +1,32 @@
+<?php
+ ini_set("memory_limit",-1);
+
+function uule($city) {
+	$secretkey = array_merge(range('A','Z'), range('a','z'), range('0','9'), array('-', '_'));
+	return trim('w+CAIQICI'.$secretkey[strlen($city)%count($secretkey)].base64_encode($city), '=');
+}
+
+$final = array();
+
+$row = 1;
+if (($handle = fopen("latest.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    	if($row==1)
+    		$final[] = array_merge($data, array('Uule parameter'));
+    	else
+        	$final[] = array_merge($data, array(uule($data[2])));
+        echo $row++."\r\n"; flush();
+    }
+    fclose($handle);
+}
+
+$fp = fopen('file.csv', 'w');
+
+foreach ($final as $fields) {
+    fputcsv($fp, $fields);
+}
+
+fclose($fp);
+
+?>
+</pre>
